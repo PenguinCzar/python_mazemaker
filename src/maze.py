0c351fadbc9380fc76fe2132560fa24b074b79db
Joseph Hampton <sniffrocks@gmail.com>
2022-05-29 23:57:35 -0500

    Feat: Add main py file

---
+++ src/maze.py
@@ -0,0 +1,14 @@
+class Maze(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.map = []
+
+    def generate_maze(self):
+        #TODO: maze generation
+        print("Not Implemented")
+        pass
+
+if __name__ == "__main__":
+    m = Maze(10, 10)
+    print(m.generate_maze())
